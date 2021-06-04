79410ca8447f4db0bb8dc992759d878493c91ee8
Edward Magno <edwardmagno1@gmail.com>
2021-06-04 10:00:44 +0800

    made layout

--- index.php
+++ index.php
@@ -8,30 +8,7 @@ require_once("./Functions/is_valid_login.php");
 if(isset($_SESSION["uuid"])){
     header("location: ./Dashboard/");
 } else {
-
-echo "
-<html>
-    <head>
-        <title>Index</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    </head>
-    <body>
-        <center>
-            <form method='POST' action=''>
-                <label for='Email'>Email:</label><br>
-                <input type='text' id='Email' name='Email' autocomplete='cc-email'>
-                <br>
-
-                <label for='Password'>Password:</label><br>
-                <input type='password' id='Password' name='Password' autocomplete='cc-password'>
-                <br><br>
-                <input type='submit' value='LOGIN' name='LOGIN'>
-            </form>
-            <a href='./Register/'>REGISTER</a>
-        </center>
-    </body>
-</html>
-";
+include('login.php');
 }
 
 if(isset($_POST["LOGIN"])){
---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Index</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    </head>
+    <body>
+        <center>
+            <form method='POST' action=''>
+                <label for='Email'>Email:</label><br>
+                <input type='text' id='Email' name='Email' autocomplete='cc-email'>
+                <br>
+
+                <label for='Password'>Password:</label><br>
+                <input type='password' id='Password' name='Password' autocomplete='cc-password'>
+                <br><br>
+                <input type='submit' value='LOGIN' name='LOGIN'>
+            </form>
+            <a href='./Register/'>REGISTER</a>
+        </center>
+    </body>
+</html>
